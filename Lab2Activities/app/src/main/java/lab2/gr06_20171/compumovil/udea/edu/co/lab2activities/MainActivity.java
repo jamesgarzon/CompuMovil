1ef47d1e141b880ea027186fb7f0591bddde6518
james garzon <james@experimentality.co>
2017-03-04 13:55:32 -0500

    Create second laboratory project and configure .gitignore to avoid upload IDE files

---
+++ Lab2Activities/app/src/main/java/lab2/gr06_20171/compumovil/udea/edu/co/lab2activities/MainActivity.java
@@ -0,0 +1,13 @@
+package lab2.gr06_20171.compumovil.udea.edu.co.lab2activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
