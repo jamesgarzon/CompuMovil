a2b168fde02a676059488765dec836c5b31674bf
james garzon <james@experimentality.co>
2017-04-15 14:36:18 -0500

    service created

---
+++ LabsCM20171/lab3services/src/main/java/co/edu/udea/gr06_20171compumovil/lab3services/RestInterface.java
@@ -0,0 +1,19 @@
+package co.edu.udea.gr06_20171compumovil.lab3services;
+
+import co.edu.udea.gr06_20171compumovil.lab3services.Pojos.User;
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Path;
+import retrofit.http.Query;
+
+/**
+ * Created by james on 4/15/17.
+ */
+
+public interface RestInterface {
+
+    @GET("/users/{user}")
+    void getUser(@Path("user") String user, Callback<User> cb);
+}
+
+
---
+++ co 2/edu/udea/gr06_20171compumovil/lab3services/Pojo/Event.java
@@ -0,0 +1,109 @@
+
+package co.edu.udea.gr06_20171compumovil.lab3services.Pojo;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Event {
+
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("description")
+    @Expose
+    private String description;
+    @SerializedName("score")
+    @Expose
+    private String score;
+    @SerializedName("picture")
+    @Expose
+    private String picture;
+    @SerializedName("resposible")
+    @Expose
+    private String resposible;
+    @SerializedName("date")
+    @Expose
+    private String date;
+    @SerializedName("location")
+    @Expose
+    private String location;
+    @SerializedName("info")
+    @Expose
+    private String info;
+    @SerializedName("id")
+    @Expose
+    private String id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getResposible() {
+        return resposible;
+    }
+
+    public void setResposible(String resposible) {
+        this.resposible = resposible;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+}
